c30a6880a862566cd6d17e688a55c3716d40fea0
rippponmangaraj <ripponmangaraj@gmail.com>
2025-07-09 16:31:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+BOOTSTRAP_SERVERS = 'localhost:9092'
+FLIGHT_TOPIC = 'flights'
+WEATHER_TOPIC = 'weather'
+OUTPUT_PATH = '/Users/ripponmangaraj/Desktop/assessment-ntt/output/enriched_flights.json'
+KAFKA_GROUP_ID = 'data-consumer-group'
